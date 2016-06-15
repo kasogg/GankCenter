21562de80b9a8c7e1b77f64172112934ac1eb461
KasoGG <kasogg@gmail.com>
2016-06-15 19:23:44 +0800

    添加LogInterceptor

--- app/src/main/java/kasogg/com/gankcenter/api/ApiFactory.java
+++ app/src/main/java/kasogg/com/gankcenter/api/ApiFactory.java
@@ -1,5 +1,8 @@
 package kasogg.com.gankcenter.api;
 
+import kasogg.com.gankcenter.BuildConfig;
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -13,14 +16,26 @@ public class ApiFactory {
 
     // @formatter:off
     private ApiFactory() {
+        OkHttpClient client = buildOKHttpClient();
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(SERVER_URL)
                 .addConverterFactory(GsonConverterFactory.create())
                 .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+                .client(client)
                 .build();
         gankApi = retrofit.create(GankApi.class);
     }
     // @formatter:on
+
+    private OkHttpClient buildOKHttpClient() {
+        OkHttpClient.Builder builder = new OkHttpClient.Builder();
+        if (BuildConfig.DEBUG) {
+            HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor();
+            loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+            builder.addInterceptor(loggingInterceptor);
+        }
+        return builder.build();
+    }
 
     public static GankApi getGankApi() {
         return Nested.instance.gankApi;
