a7564a1f197b939f36e2ace2184cd56e68220e49
KasoGG <kasogg@gmail.com>
2016-06-21 21:28:08 +0800

    更新

---
+++ app/src/main/java/kasogg/com/gankcenter/entity/Meizhi.java
@@ -0,0 +1,29 @@
+package kasogg.com.gankcenter.entity;
+
+/**
+ * Created by KasoGG on 2016/5/8.
+ */
+public class Meizhi {
+
+    /**
+     * _id : 572c146a67765974f885c01e
+     * createdAt : 2016-05-06T11:50:02.319Z
+     * desc : 无版权
+     * publishedAt : 2016-05-06T12:04:47.203Z
+     * source : chrome
+     * type : 福利
+     * url : http://ww4.sinaimg.cn/large/610dc034jw1f3litmfts1j20qo0hsac7.jpg
+     * used : true
+     * who : 代码家
+     */
+
+    public String _id;
+    public String createdAt;
+    public String desc;
+    public String publishedAt;
+    public String source;
+    public String type;
+    public String url;
+    public boolean used;
+    public String who;
+}
